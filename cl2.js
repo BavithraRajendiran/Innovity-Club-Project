cb03916d3b03ebcb7945b8adaa784b8fd906fe5e
Bavithra <71627208+BavithraRajendiran@users.noreply.github.com>
2021-07-22 14:18:27 +0530

    Add files via upload

---
+++ cl2.js
@@ -0,0 +1,15 @@
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
+document.querySelector("#show-login").addEventListener("click",function(){
+  document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+  document.querySelector(".popup").classList.remove("active");
+});
+
+
